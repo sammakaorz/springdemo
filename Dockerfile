46de6ca0233253a663c0a3bb44d77ce0d6681bce
sammak <sammakaorz@hotmail.com>
2024-03-13 13:33:10 -0400

    updated Dockerfile, pom.xml, manifest file and SpringdemoApplication.java

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,11 @@ FROM maven:latest AS build
 COPY src /home/app/src
 COPY pom.xml /home/app
 RUN mvn -f /home/app/pom.xml clean package
-
+RUN ls /home/app/target/
 #
 # Package stage
 #
 FROM openjdk:17
-COPY --from=build /home/app/target/springdemo-0.0.1-SNAPSHOT.jar /usr/local/lib/demo.jar
+COPY --from=build /home/app/target/springdemo.jar /usr/local/lib/demo.jar
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/usr/local/lib/demo.jar"]
--- src/main/java/com/example/springdemo/SpringdemoApplication.java
+++ src/main/java/com/example/springdemo/SpringdemoApplication.java
@@ -2,12 +2,20 @@ package com.example.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringdemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringdemoApplication.class, args);
-	}
+        @GetMapping("/message")
+        public String welcome(){
+                return "Congratulation you successfully deployed your application to kubernetes !!";
+        }
+
+        public static void main(String[] args) {
+                SpringApplication.run(SpringdemoApplication.class, args);
+        }
 
 }
